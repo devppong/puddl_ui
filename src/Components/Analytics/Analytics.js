e02427d938ac5885822b17d7350dc1f1a5bbd151
BharathReddy294 <bharathreddydevarapally@gmail.com>
2023-05-18 23:04:43 +0530

    -

--- src/Components/Analytics/Analytics.js
+++ src/Components/Analytics/Analytics.js
@@ -141,206 +141,202 @@ export default function Analytics() {
 
 	return (
 		<main className="bg-slate-50 p-6 sm:p-10 w-min-full">
-			<Grid className='gap-6' numColsLg={12}>
-				<Col numColSpanLg={2}>
-					<Metric>Analytics</Metric>
-				</Col>
+			<Flex
+				style={{
+					alignItems: "center",
+					flexWrap: "wrap",
+					justifyContent: "space-evenly",
+				}}
+			>
+				<Metric>Analytics</Metric>
 				{apiKeyStatus === "success" ? (
-					<Col numColSpanLg={2}>
-						<Badge
-							className="max-w-sm "
-							color="green"
-							style={{ margin: "10px" }}
+					<Badge
+						className="max-w-sm "
+						color="green"
+						style={{ margin: "10px" }}
+					>
+						<span
+							style={{
+								display: "flex",
+								alignItems: "center",
+								padding: "2px",
+							}}
 						>
 							<span
-								style={{
-									display: "flex",
-									alignItems: "center",
-									padding: "2px",
-								}}
+								className="animate-ping bg-green-50 opacity-75"
+								style={{ width: "10px" }}
 							>
-								<span
-									className="animate-ping bg-green-50 opacity-75"
-									style={{ width: "10px" }}
-								>
-									<StatusOnlineIcon />
-								</span>
-								<span className="ml-2">Active</span>
+								<StatusOnlineIcon />
 							</span>
-						</Badge>
-					</Col>
+							<span className="ml-2">Active</span>
+						</span>
+					</Badge>
 				) : (
-					<Col numColSpanLg={2}>
-						<Badge
-							className="max-w-sm "
-							color="red"
-							style={{ margin: "10px" }}
+					<Badge
+						className="max-w-sm "
+						color="red"
+						style={{ margin: "10px" }}
+					>
+						<span
+							style={{
+								display: "flex",
+								alignItems: "center",
+								padding: "2px",
+							}}
 						>
 							<span
-								style={{
-									display: "flex",
-									alignItems: "center",
-									padding: "2px",
-								}}
+								className="animate-ping bg-red-50 opacity-75"
+								style={{ width: "10px" }}
 							>
-								<span
-									className="animate-ping bg-red-50 opacity-75"
-									style={{ width: "10px" }}
-								>
-									<StatusOfflineIcon />
-								</span>
-								<span className="ml-2">Inactive</span>
+								<StatusOfflineIcon />
 							</span>
-						</Badge>
-					</Col>
+							<span className="ml-2">Inactive</span>
+						</span>
+					</Badge>
 				)}
-				<Col numColSpanLg={4}>
-					<DateRangePicker
-						style={{ margin: "10px", maxWidth: "350px" }}
-						className="max-w-sm"
-						enableDropdown={true}
-						value={state.date_range}
-						onValueChange={handleUpdateDateRange}
-						options={state.datePickerOptions}
-						dropdownPlaceholder="Timeline"
+				<DateRangePicker
+					style={{ margin: "10px", maxWidth: "350px" }}
+					className="max-w-sm"
+					enableDropdown={true}
+					value={state.date_range}
+					onValueChange={handleUpdateDateRange}
+					options={state.datePickerOptions}
+					dropdownPlaceholder="Timeline"
+				/>
+				<MultiSelectBox
+					className="max-w-sm space-y-6 gap-6"
+					onValueChange={handleUpdateFilters}
+					value={state.filters}
+					style={{ margin: "10px", width: "150px" }}
+					placeholder="Model Types"
+				>
+					<MultiSelectBoxItem
+						value="Instruct models"
+						text="Instruct models"
 					/>
-				</Col>
-				<Col numColSpanLg={2}>
-					<MultiSelectBox
-						className="max-w-sm space-y-6 gap-6"
-						onValueChange={handleUpdateFilters}
-						value={state.filters}
-						style={{ margin: "10px", width: "150px" }}
-						placeholder="Model Types"
-					>
-						<MultiSelectBoxItem
-							value="Instruct models"
-							text="Instruct models"
+					<MultiSelectBoxItem
+						value="Chat models"
+						text="Chat models"
+					/>
+					<MultiSelectBoxItem value="GPT-4" text="GPT-4" />
+					<MultiSelectBoxItem
+						value="Fine-tuned models"
+						text="Fine-tuned models"
+					/>
+					<MultiSelectBoxItem
+						value="Embedding models"
+						text="Embedding models"
+					/>
+					<MultiSelectBoxItem
+						value="Image models"
+						text="Image models"
+					/>
+					<MultiSelectBoxItem
+						value="Audio models"
+						text="Audio models"
+					/>
+				</MultiSelectBox>
+				<SelectBox
+					className="max-w-sm space-y-6 gap-6"
+					onValueChange={handleSelectedUser}
+					value={seletedUser}
+					style={{ margin: "10px", width: "150px" }}
+					placeholder="Users"
+				>
+					<SelectBoxItem value="All Users" text="All Users" />
+					{state.org_users.map((user) => (
+						<SelectBoxItem
+							value={user[0]}
+							text={user[1]}
+							key={user[0]}
 						/>
-						<MultiSelectBoxItem
-							value="Chat models"
-							text="Chat models"
+					))}
+				</SelectBox>
+			</Flex>
+			<Flex
+				style={{
+					justifyContent: "space-evenly",
+					flexWrap: "wrap",
+					alignItems: "baseline",
+					margin: "1rem auto",
+					gap: "2rem",
+				}}
+			>
+				<Popover title="Your OpenAI key is stored locally">
+					<label>OpenAI Key: </label>
+					<span>
+						<Input.Password
+							status={apiKeyStatus}
+							className="max-w-sm"
+							placeholder="OpenAI key(stored locally)"
+							onChange={onChangekey}
+							value={apiKey}
+							size="large"
+							style={{ width: "320px" }}
 						/>
-						<MultiSelectBoxItem value="GPT-4" text="GPT-4" />
-						<MultiSelectBoxItem
-							value="Fine-tuned models"
-							text="Fine-tuned models"
-						/>
-						<MultiSelectBoxItem
-							value="Embedding models"
-							text="Embedding models"
-						/>
-						<MultiSelectBoxItem
-							value="Image models"
-							text="Image models"
-						/>
-						<MultiSelectBoxItem
-							value="Audio models"
-							text="Audio models"
-						/>
-					</MultiSelectBox>
-				</Col>
-				<Col numColSpanLg={2}>
-					<SelectBox
-						className="max-w-sm space-y-6 gap-6"
-						onValueChange={handleSelectedUser}
-						value={seletedUser}
-						style={{ margin: "10px", width: "150px" }}
-						placeholder="Users"
-					>
-						<SelectBoxItem value="All Users" text="All Users" />
-						{state.org_users.map((user) => (
-							<SelectBoxItem
-								value={user[0]}
-								text={user[1]}
-								key={user[0]}
-							/>
-						))}
-					</SelectBox>
-				</Col>
-			</Grid>
-			<Grid className='gap-6' numColsLg={12}>
-				<Col numColSpanLg={5}>
-					<Popover title="Your OpenAI key is stored locally">
-						<label>OpenAI API Key: </label>
-						<span>
-							<Input.Password
-								status={apiKeyStatus}
-								className="max-w-sm"
-								placeholder="OpenAI key(stored locally)"
-								onChange={onChangekey}
-								value={apiKey}
-								size="large"
-								style={{ width: "320px" }}
-							/>
-							<sub
-								style={{
-									display: "block",
-									textAlign: "center",
-									marginTop: "1em",
-								}}
+						<sub
+							style={{
+								display: "block",
+								textAlign: "center",
+								marginTop: "1em",
+							}}
+						>
+							click{" "}
+							<a
+								href="https://platform.openai.com/account/api-keys"
+								style={{ color: "blue" }}
+								target="_blank"
+								rel="noreferrer"
 							>
-								click{" "}
-								<a
-									href="https://platform.openai.com/account/api-keys"
-									style={{ color: "blue" }}
-									target="_blank"
-									rel="noreferrer"
-								>
-									here
-								</a>{" "}
-								for your OpenAI key
-							</sub>
-						</span>
-					</Popover>
-				</Col>
-				<Col numColSpanLg={5}>
-					<Popover title="Your OrgID is stored locally">
-						<label>OpenAI Org Key: </label>
-						<span>
-							<Input.Password
-								className="max-w-sm"
-								placeholder="OrgID(stored locally)"
-								onChange={onChangeOrgID}
-								value={OrgID}
-								size="large"
-								style={{ width: "320px" }}
-							/>
-							<sub
-								style={{
-									display: "block",
-									textAlign: "center",
-									marginTop: "1em",
-								}}
+								here
+							</a>{" "}
+							for your OpenAI key
+						</sub>
+					</span>
+				</Popover>
+				<Popover title="Your OrgID is stored locally">
+					<label>OpenAI OrgID: </label>
+					<span>
+						<Input.Password
+							className="max-w-sm"
+							placeholder="OrgID(stored locally)"
+							onChange={onChangeOrgID}
+							value={OrgID}
+							size="large"
+							style={{ width: "320px" }}
+						/>
+						<sub
+							style={{
+								display: "block",
+								textAlign: "center",
+								marginTop: "1em",
+							}}
+						>
+							click{" "}
+							<a
+								href="https://platform.openai.com/account/org-settings"
+								style={{ color: "blue" }}
+								target="_blank"
+								rel="noreferrer"
 							>
-								click{" "}
-								<a
-									href="https://platform.openai.com/account/org-settings"
-									style={{ color: "blue" }}
-									target="_blank"
-									rel="noreferrer"
-								>
-									here
-								</a>{" "}
-								for your OpenAI OrgID
-							</sub>
-						</span>
-					</Popover>
-				</Col>
-				<Col numColSpanLg={2}>
-					<Toggle
-						defaultValue={false}
-						onValueChange={(value) => {
-							console.log(value)
-							setSelectedUSD(value);
-							updateSelectedCurrency(state, dispatch, value);
-						}}
-					>
-						<ToggleItem value={true} text="USD" />
-						<ToggleItem value={false} text="Local Currency" />
-					</Toggle>
-				</Col>
-			</Grid>
+								here
+							</a>{" "}
+							for your OpenAI OrgID
+						</sub>
+					</span>
+				</Popover>
+				<Toggle
+					defaultValue={false}
+					onValueChange={(value) => {
+						console.log(value)
+						setSelectedUSD(value);
+						updateSelectedCurrency(state,dispatch,value);
+					}}
+				>
+					<ToggleItem value={true} text="USD" />
+					<ToggleItem value={false} text="Local Currency" />
+				</Toggle>
+			</Flex>
 			<Grid numColsLg={3} className="mt-6 gap-6 h-full">
 				<Col numColSpanLg={2}>
 					{<Barchart state={state} dispatch={dispatch} />}
@@ -363,36 +359,34 @@ export default function Analytics() {
 				</Col>
 			</Grid>
 
-			{
-				seletedUser === "All Users" && (
-					<>
-						<Typography
-							variant="h4"
-							style={{ margin: "4rem auto 2rem 1rem" }}
-						>
-							User Level Breakdown
-						</Typography>
+			{seletedUser === "All Users" && (
+				<>
+					<Typography
+						variant="h4"
+						style={{ margin: "4rem auto 2rem 1rem" }}
+					>
+						User Level Breakdown
+					</Typography>
 
-						<Grid numColsLg={3} className="mt-6 gap-6">
-							<Col numColSpanLg={1}>
-								<UserLevelCost state={state} dispatch={dispatch} />
-							</Col>
-							<Col numColSpanLg={1}>
-								<UserLevelRequests
-									state={state}
-									dispatch={dispatch}
-								/>
-							</Col>
-							<Col numColSpanLg={1}>
-								<UserLevelTokens
-									state={state}
-									dispatch={dispatch}
-								/>
-							</Col>
-						</Grid>
-					</>
-				)
-			}
-		</main >
+					<Grid numColsLg={3} className="mt-6 gap-6">
+						<Col numColSpanLg={1}>
+							<UserLevelCost state={state} dispatch={dispatch} />
+						</Col>
+						<Col numColSpanLg={1}>
+							<UserLevelRequests
+								state={state}
+								dispatch={dispatch}
+							/>
+						</Col>
+						<Col numColSpanLg={1}>
+							<UserLevelTokens
+								state={state}
+								dispatch={dispatch}
+							/>
+						</Col>
+					</Grid>
+				</>
+			)}
+		</main>
 	);
 }
